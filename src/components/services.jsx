4079263f89c7be99dc996dff81798a4756ef354c
Yvelt Desamours <91551242+yDesamours@users.noreply.github.com>
2022-04-01 23:47:27 +0000

    services added

--- src/components/services.jsx
+++ src/components/services.jsx
@@ -1,11 +1,15 @@
 import Service from './service';
 import Box from './box';
 import prescription from '../medias/prescription.jpg';
+import qr from '../medias/qr.jpg';
+import carte from '../medias/carte.jpg';
 
 function services (props){
   return(
     <Box className='services'>
-          <Service to='/write' caption='rediger une prescription' image={prescription} />
+      <Service to='/write' caption='rediger une prescription' image={prescription} />
+      <Service to='/read' caption='lire une prescription' image={qr} />
+      <Service to='/find' caption='trouver une pharmacie' image={carte} />
     </Box>
   );
 }
