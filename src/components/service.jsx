47fde8d850dd15989ee42cc41e863c36861aadf8
Yvelt Desamours <91551242+yDesamours@users.noreply.github.com>
2022-04-01 21:08:08 +0000

    create service component

---
+++ src/components/service.jsx
@@ -0,0 +1,16 @@
+import { Link } from 'react-router-dom';
+
+function service(props) {
+  return(
+    <article className='service'>
+      <Link to={props.to}>
+        <figure>
+          <img src={props.image} alt={props.service}
+          <figcaption>{props.caption}</figcaption>
+        </figure>
+      </Link>
+    </article>
+  );
+}
+
+export default props;
