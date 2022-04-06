eca474b4111a560353383fb562899e4a34c61ef8
Yvelt Desamours <91551242+yDesamours@users.noreply.github.com>
2022-04-06 02:18:18 +0000

    update

--- src/components/lecteur.jsx
+++ src/components/lecteur.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import Box from './box';
+import '../styles/lecteur.css';
 import Button from './button';
 
 class Lecteur extends React.Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      file : '',
+    }
+  }
   render(){
     return(
       <Box className="lecteur">
-        <input type="file" id="file"/>
-        <label for="file">Choisir un fichier</label>
+        <div className="file_input">
+          <input type="file" id="file"/>
+          <label for="file"><p>Choisir un fichier</p></label>
+          <p>{ this.state.file }</p>
+        </div>
       </Box>
     );
   }
