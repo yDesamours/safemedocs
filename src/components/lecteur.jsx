9b250b0978ebf487656b4d9c899e13e340658b9f
Yvelt Desamours <91551242+yDesamours@users.noreply.github.com>
2022-04-06 03:25:52 +0000

    update

--- src/components/lecteur.jsx
+++ src/components/lecteur.jsx
@@ -10,16 +10,38 @@ class Lecteur extends React.Component{
     this.state = {
       file : '',
     }
+    this.handleFile = this.handleFile.bind(this)
+    this.handleChange = this.handleChange.bind(this)
   }
+  
+  handleChange(e){
+    if(e.target.files.length != 0 ) 
+      this.setState( {file : e.target.files[0].name} )
+  }
+
+  handleFile(){
+    let qr = new Html5Qrcode("reader")
+    let file = document.getElementById('file').files[0]
+
+    qr.scanFile(file, false)
+    .then(decodedText => {
+      console.log(decodedText);
+    })
+    .catch(err => {
+      console.log(`Error scanning file. Reason: ${err}`)
+    });
+  }
+  
   render(){
-    let qr = new Html5Qrcode()
     return(
       <Box className="lecteur">
         <div className="file_input">
-          <input type="file" id="file"/>
-          <label for="file"><p>Choisir un fichier</p></label>
+          <input type="file" id="file" onChange={this.handleChange} />
+          <label htmlFor="file"><p>Choisir un fichier</p></label>
           <p>{ this.state.file }</p>
+          <button onClick = {this.handleFile} disabled= {!this.state.file}>Retreive</button>
         </div>
+        <div id="reader"></div>
       </Box>
     );
   }
