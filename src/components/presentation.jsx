b09e8e449fc3ce3db3bf58699da1d6e709c46129
Yvelt Desamours <91551242+yDesamours@users.noreply.github.com>
2022-04-08 02:44:19 +0000

    add react-responsive

--- src/components/presentation.jsx
+++ src/components/presentation.jsx
@@ -1,14 +1,18 @@
+import { useMediaQuery } from 'react-responsive';
 import '../styles/presentation.css';
+import React from 'react';
 
-function presentation(props) {
+const Presentation = (props)=> {
+  let isMobile = useMediaQuery({ query : '(max-width : 400px)'})
+  console.log(isMobile)
   return(
     <div className="presentation">
       <div className="presentation_text"><p>{props.text}</p></div>
-      <figure className="presentation_image">
+      <figure className="presentation_image" >
         <img src={props.image} alt="illustration" />
       </figure>
     </div>
   );
 }
 
-export default presentation;
+export default Presentation;
